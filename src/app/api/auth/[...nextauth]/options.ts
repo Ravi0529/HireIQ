7761cc2ae7fa0ca4ea765dab370305d4a59ca336
Ravi0529 <150603739+Ravi0529@users.noreply.github.com>
2025-08-16 14:06:45 +0530

    redis-stack-init

--- src/app/api/auth/[...nextauth]/options.ts
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -4,6 +4,7 @@ import GoogleProvider from "next-auth/providers/google";
 import prisma from "@/lib/prisma";
 import bcrypt from "bcryptjs";
 import { Role } from "@prisma/client";
+import redis from "@/lib/redis";
 
 export const authOptions: NextAuthOptions = {
   providers: [
@@ -17,6 +18,27 @@ export const authOptions: NextAuthOptions = {
       authorize: async (credentials) => {
         if (!credentials?.email || !credentials?.password) {
           throw new Error("Email and password are required");
+        }
+
+        const cachedUser = await redis.get(`user:${credentials.email}`);
+        if (cachedUser) {
+          const user = JSON.parse(cachedUser);
+
+          const isPasswordCorrect = await bcrypt.compare(
+            credentials.password,
+            user.password
+          );
+          if (!isPasswordCorrect)
+            throw new Error("Incorrect email or password");
+
+          return {
+            id: user.id,
+            role: user.role,
+            firstName: user.firstName,
+            lastName: user.lastName,
+            email: user.email,
+            image: user.image,
+          };
         }
 
         const user = await prisma.user.findUnique({
@@ -35,6 +57,13 @@ export const authOptions: NextAuthOptions = {
         if (!isPasswordCorrect) {
           throw new Error("Incorrect email or password");
         }
+
+        await redis.set(
+          `user:${user.email}`,
+          JSON.stringify(user),
+          "EX",
+          60 * 60 * 24 * 30
+        );
 
         return {
           id: user.id,
